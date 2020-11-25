e9dc23d731df95c687cc4417bbcbd8ac2796ff0e
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-25 17:52:54 +0300

    init

---
+++ src/test/java/ru/netology/rest/PostManApiTest.java
@@ -0,0 +1,42 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostManApiTest {
+    @Test
+    void shouldReturnQueryAndGetQuery() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("HELLO")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("HELLO"))
+        ;
+
+    }
+
+    @Test
+    void shouldReturnRusQueryAndGetRusQuery(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("привет")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("привет"))
+        ;
+    }
+
+
+}
+
+
+
+
